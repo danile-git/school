303933515b638208111b27106d382dd32cd18aaf
danile-git <shaobodou@hotmail.com>
2019-05-19 21:25:46 +0800

    vgods

--- pages/circle/circle.js
+++ pages/circle/circle.js
@@ -8,7 +8,7 @@ Page({
    */
   data: {
     title:"新闻中心",
-    baseUrl: app.globalData.baseUrl,
+    baseUrl: "http://localhost:8080/orange",
     headerImg:app.globalData.baseUrl + "/images/tt1.png",
     order: [{ "img": "/images/h3.jpg", "desc": "通过漫画表现不同场景的日常交际，培养儿童早期听说、阅读、想象能力。", "time": "2019年5月4日", "url": "http://www.baidu.com" }, { "img": "/images/h1.png", "desc": "通过漫画表现不同场景的日常交际，培养儿童早期听说、阅读、想象能力。", "time": "2019年5月4日", "url":"https://weibo.com/?c=spr_qdhz_bd_360jsllqcj_weibo_001" }]
   },
